d4b77744e8850e7d928837817029139855ff8685
青木 <qingmu@2dfire.com>
2018-02-26 15:39:24 +0800

    style: update command

--- TDFModuleKit/Classes/TDFModule.h
+++ TDFModuleKit/Classes/TDFModule.h
@@ -18,9 +18,13 @@
 
  - TDFModulePriorityVeryLow: 极底
  - TDFModulePriorityLow: 低
+ 安排给弱业务，业务模块
+ 
  - TDFModulePriorityMedium: 中
  - TDFModulePriorityHigh: 高
  - TDFModulePriorityVeryHigh: 极高
+ 安排给基础模块（有些基础模块每次依赖都需要手动调用初始化方法，建议分成 Core / Initializer subspec，后者中只有一个类继承TDFModule，这样直接依赖模块时，初始化代码的编写就可以去掉了）
+ 这种情况下，TDFModule子类中，最好不要存在硬编码，使用变量或配置文件配置，这样才能让各业务线通用
  */
 typedef NS_ENUM(NSInteger, TDFModulePriority) {
     TDFModulePriorityVeryLow = 0,
